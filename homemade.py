6c3bb5cc64981b2a108a25a25884e74b88638ecc
donald <donald.flynn@skyrad.co.uk>
2024-06-25 10:05:37 +0100

    Added TimeManager class for improved time usage heuristics

--- homemade.py
+++ homemade.py
@@ -10,22 +10,23 @@ import random
 from lib.engine_wrapper import MinimalEngine
 from lib.types import MOVE, HOMEMADE_ARGS_TYPE
 import logging
-
+from lib import model
+from time_management import TimeManager
+import time
 
 # Use this logger variable to print messages to the console or log files.
 # logger.info("message") will always print "message" to the console or log file.
 # logger.debug("message") will only print "message" if verbose logging is enabled.
 logger = logging.getLogger(__name__)
 
-
 LEELA_PATH = "/app/engines/lc0"
 STOCKFISH_PATH = "/app/engines/stockfish"
 
 MAIA_CANDIDATE_MOVES = 3
 
+
 class ExampleEngine(MinimalEngine):
     """An example engine that all homemade engines inherit."""
-
     pass
 
 
@@ -34,6 +35,7 @@ class Hybrid(ExampleEngine):
         super(Hybrid, self).__init__(*args, **kwargs)
         self._leela_engine = chess.engine.SimpleEngine.popen_uci(LEELA_PATH)
         self._stockfish_engine = chess.engine.SimpleEngine.popen_uci(STOCKFISH_PATH)
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         """
         Wrap the exit method of ExampleEngine to ensure we exit the engines properly in case of an
@@ -43,43 +45,23 @@ class Hybrid(ExampleEngine):
         self._stockfish_engine.__exit__(exc_type, exc_val, exc_tb)
         super(Hybrid, self).__exit__(exc_type, exc_val, exc_tb)
 
-    def search(self, board: chess.Board, *args: HOMEMADE_ARGS_TYPE) -> PlayResult:
-        info_list = self._leela_engine.analyse(board, chess.engine.Limit(time=2), multipv=MAIA_CANDIDATE_MOVES)
-        moves_list = [info['pv'][0] for info in info_list]
-        logger.info("Candidate moves received from maia: " + ", ".join([str(move) for move in moves_list]))
-        play_result = self._stockfish_engine.play(board, chess.engine.Limit(time=0.1), root_moves=moves_list)
-        logger.info("Final move decided from stockfish: %s", str(play_result.move))
+    def search(self, board: chess.Board, time_limit: Limit, game: model.Game, *args: HOMEMADE_ARGS_TYPE) -> PlayResult:
+        time_manager = TimeManager(board, game)
+        decide_time = time_manager.get_initial_time()
+        logger.info("Using time: %s to search for a move", decide_time)
+        play_result = self.get_move(board, decide_time=decide_time)
+        time.sleep(time_manager.get_pause_time(play_result.move))
         return play_result
 
-
-# Bot names and ideas from tom7's excellent eloWorld video
-
-class RandomMove(ExampleEngine):
-    """Get a random move."""
-
-    def search(self, board: chess.Board, *args: HOMEMADE_ARGS_TYPE) -> PlayResult:
-        """Choose a random move."""
-        return PlayResult(random.choice(list(board.legal_moves)), None)
-
-
-class Alphabetical(ExampleEngine):
-    """Get the first move when sorted by san representation."""
-
-    def search(self, board: chess.Board, *args: HOMEMADE_ARGS_TYPE) -> PlayResult:
-        """Choose the first move alphabetically."""
-        moves = list(board.legal_moves)
-        moves.sort(key=board.san)
-        return PlayResult(moves[0], None)
-
-
-class FirstMove(ExampleEngine):
-    """Get the first move when sorted by uci representation."""
-
-    def search(self, board: chess.Board, *args: HOMEMADE_ARGS_TYPE) -> PlayResult:
-        """Choose the first move alphabetically in uci representation."""
-        moves = list(board.legal_moves)
-        moves.sort(key=str)
-        return PlayResult(moves[0], None)
+    def get_move(self, board: chess.Board, decide_time: float) -> PlayResult:
+        stockfish_time = 0.1
+        maia_time = decide_time - stockfish_time
+        info_list = self._leela_engine.analyse(board, chess.engine.Limit(time=maia_time), multipv=MAIA_CANDIDATE_MOVES)
+        moves_list = [info['pv'][0] for info in info_list]
+        logger.info("Candidate moves received from maia: " + ", ".join([str(move) for move in moves_list]))
+        play_result = self._stockfish_engine.play(board, chess.engine.Limit(time=stockfish_time), root_moves=moves_list)
+        logger.info("Final move decided from stockfish: %s", str(play_result.move))
+        return play_result
 
 
 class ComboEngine(ExampleEngine):
@@ -89,7 +71,8 @@ class ComboEngine(ExampleEngine):
     This engine demonstrates how one can use `time_limit`, `draw_offered`, and `root_moves`.
     """
 
-    def search(self, board: chess.Board, time_limit: Limit, ponder: bool, draw_offered: bool, root_moves: MOVE) -> PlayResult:
+    def search(self, board: chess.Board, time_limit: Limit, ponder: bool, draw_offered: bool,
+               root_moves: MOVE) -> PlayResult:
         """
         Choose a move using multiple different methods.
 
---
+++ time_management.py
@@ -0,0 +1,141 @@
+from typing import Optional
+import logging
+import chess
+from lib import model
+import random
+
+logger = logging.getLogger(__name__)
+
+
+value_mapping = {
+    chess.PAWN: 1,
+    chess.KNIGHT: 3,
+    chess.BISHOP: 3,
+    chess.ROOK: 5,
+    chess.QUEEN: 8,
+    chess.KING: 0,
+}
+
+color_mapping = {
+    chess.WHITE: 1,
+    chess.BLACK: -1
+}
+
+
+class TimeManager:
+    def __init__(self, board: chess.Board, game: model.Game):
+        self.game_state = _GameState(board, game)
+
+    def get_initial_time(self) -> float:
+        """Returns actual thinking time of the move"""
+        my_time, my_inc = self.game_state.time_left
+        if my_time < 60:
+            return 0.5
+        else:
+            return 1
+
+    def get_pause_time(self, move: chess.Move) -> float:
+        """Returns fake thinking time after making the move"""
+        my_time, my_inc = self.game_state.time_left
+        if my_time < 10:
+            result = 0
+        elif 10 <= my_time < 60:
+            result = random.uniform(0, self._get_complexity_measure())
+        elif my_time >= 60:
+            # https://www.desmos.com/calculator/f5cgemje2s
+            # Random weight between 0 and 1 weighted towards the bottom, but with a long tail
+            rand_weight = min(random.lognormvariate(-1, 1), 1)
+            return rand_weight * 30 * self._get_complexity_measure()
+
+    def _get_complexity_measure(self) -> float:
+        """Returns a value between 0 and 1 corresponding roughly to how complex the game is"""
+        complexity_percent = 0
+
+        # 50 Points from how many legal moves there are:
+        complexity_percent += min(self.game_state.get_legal_moves_count() * 1.5, 50)
+
+        # 25 Points for if classified as middlegame instead of endgame:
+        if not self.game_state.is_endgame():
+            complexity_percent += 25
+
+        # 25 Points for if the game is equal or not
+        if self.game_state.is_equal():
+            complexity_percent += 25
+
+        return complexity_percent / 100
+
+
+class _LastPosition:
+    def __init__(self, board: chess.Board):
+        self.board = board
+        self.last_move: Optional[chess.Move] = None
+
+    def __enter__(self):
+        """
+        It's up to the caller to ensure that there is a previous move.
+        """
+        self.last_move = self.board.pop()
+        return self.board, self.last_move
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.board.push(self.last_move)
+
+
+class _GameState:
+    def __init__(self, board: chess.Board, game: model.Game):
+        self.board = board
+        self.game = game
+        self._is_white = game.my_color == "white"
+
+    def is_endgame(self) -> bool:
+        has_white_queen = len(self.board.pieces(chess.QUEEN, chess.WHITE)) == 0
+        has_black_queen = len(self.board.pieces(chess.QUEEN, chess.BLACK)) == 0
+        if has_white_queen and has_black_queen:
+            return False
+        if self.total_material_value > 20:
+            return False
+
+    def is_equal(self) -> bool:
+        return abs(self.material_diff) <= 5
+
+    def is_recapture(self, current_move: chess.Move) -> bool:
+        if self.is_first_move:
+            return False
+        with _LastPosition(self.board) as context:
+            last_pos, last_move = context
+            return (
+                    current_move.to_square == last_move.to_square and
+                    last_pos.piece_at(last_move.to_square) is not None
+            )
+
+    def get_legal_moves_count(self) -> int:
+        return self.board.legal_moves.count()
+
+    @property
+    def time_left(self) -> tuple[float, float]:
+        return self.game.my_remaining_time().total_seconds(), self.game.clock_increment
+
+    @property
+    def material_diff(self):
+        return sum(
+            value_mapping[piece.piece_type] * color_mapping[piece.color]
+            for square in chess.SQUARES
+            if (piece := self.board.piece_at(square)) is not None
+        )
+
+    @property
+    def total_material_value(self):
+        return sum(
+            value_mapping[piece.piece_type]
+            for square in chess.SQUARES
+            if (piece := self.board.piece_at(square)) is not None
+        )
+
+    @property
+    def relative_material_diff(self):
+        return self.material_diff if self._is_white else -self.material_diff
+
+    @property
+    def is_first_move(self):
+        return self.board.move_stack == []
+
